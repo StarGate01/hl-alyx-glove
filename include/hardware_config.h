c7ad1749ea4e2c258a743fe8807d6a128bb6a7a9
Christoph Honal <christoph.honal@web.de>
2021-02-04 18:48:17 +0100

    Implement sensors

--- include/hardware_config.h
+++ include/hardware_config.h
@@ -4,15 +4,23 @@
 // LED matrices
 #define LED_DIN 9
 #define LED_CLK 11
-#define LED0_CS 10
+#define LED_CS  10
 
-//LEDs
-#define LED1 7
-#define LED2 8
-#define LED3 2
+// LEDs
+#define LED_CE 3
+#define LED_EX 4
+#define LED_S1 7
+#define LED_S2 8
+
+// Sensor
+#define ACC_X A2
+#define ACC_Y A1
+#define ACC_Z A0
+#define BTN_1 6
+#define BTN_2 5
 
 // LCD display
-#define LCD_WIDTH 128
+#define LCD_WIDTH  128
 #define LCD_HEIGHT 64
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -9,26 +9,57 @@
 
 
 // Display drivers
-LedControl lc(LED_DIN, LED_CLK, LED0_CS, 2);
+LedControl lc(LED_DIN, LED_CLK, LED_CS, 2);
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
 
 // Heart animation frame indices
-int indices[6] = { 0, 1, 2, 3, 2, 1 };
+#define HEART_ANIM_SIZE 6
+int heart_idx[HEART_ANIM_SIZE] = { 0, 1, 2, 3, 2, 1 };
 
 // Resin count
 unsigned short resin = 0;
 
 
+// Various parameters
+
+#define FRAME_TIME 700
+#define HEART_TIME 100
+#define LEDMAT_NUM 3
+#define LEDMAT_BRIGHTNESS 4
+#define SENSOR_SAMPLES 8
+
 // Core logic
 
-int s, i, k;
+unsigned long frame_time, heart_time;
+float acc_x, acc_y, acc_z;
+
+float read_sensor(int pin)
+{
+    int raw = 0;
+    for(int i=0; i<SENSOR_SAMPLES; i++) 
+    {
+        raw += analogRead(pin);
+    }
+    return (float)map(raw, 0, 675 * SENSOR_SAMPLES, -3000, 3000) / 1000.0f;
+}
 
 void setup() 
 {
+    // Debugging
+    Serial.begin(9600);
+
     // LEDs
-    pinMode(LED1, OUTPUT);
-    pinMode(LED2, OUTPUT);
-    pinMode(LED3, OUTPUT);
+    pinMode(LED_CE, OUTPUT);
+    pinMode(LED_EX, OUTPUT);
+    pinMode(LED_S1, OUTPUT);
+    pinMode(LED_S2, OUTPUT);
+
+    // Sensor
+    pinMode(ACC_X, INPUT);
+    pinMode(ACC_Y, INPUT);
+    pinMode(ACC_Z, INPUT);
+    pinMode(BTN_1, INPUT);
+    pinMode(BTN_2, INPUT);
 
     // LCD
     u8g2.begin();
@@ -36,16 +67,34 @@ void setup()
 
     // Matrices
     delay(500);
-    for(i=0; i<3; i++)
+    for(int i=0; i<LEDMAT_NUM; i++)
     {
         lc.shutdown(i, false);
-        lc.setIntensity(i, 4);
+        lc.setIntensity(i, LEDMAT_BRIGHTNESS);
         lc.clearDisplay(i);
     }
 }
 
 void loop() 
 {
+    frame_time = millis();
+
+    // Read sensor
+    acc_x = read_sensor(ACC_X);
+    acc_y = read_sensor(ACC_Y);
+    acc_z = read_sensor(ACC_Z);
+    Serial.print("X: ");
+    Serial.print(acc_x);
+    Serial.print(" Y: ");
+    Serial.print(acc_y);
+    Serial.print(" Z: ");
+    Serial.print(acc_z);
+    Serial.print(" B1: ");
+    Serial.print(digitalRead(BTN_1) == HIGH? 1:0);
+    Serial.print(" B2: ");
+    Serial.print(digitalRead(BTN_2) == HIGH? 1:0);
+    Serial.print("\n");
+
     // Draw LCD image
     u8g2.clearBuffer();
     u8g2.drawXBMP(ball_left, ball_top, ball_width, ball_height, ball_bits);
@@ -55,17 +104,29 @@ void loop()
     u8g2.sendBuffer();
 
     // Render heart animation
-    for(s=0; s<6; s++)
+    for(int s=0; s<6; s++)
     {
-        for(i=0; i<3; i++) for(k=0; k<8; k++) lc.setRow(i, k, heart[indices[(s + i) % 6]][k]);
-        delay(100);
+        heart_time = millis();
+        for(int i=0; i<LEDMAT_NUM; i++) 
+        {
+            for(int k=0; k<8; k++) 
+            {
+                lc.setRow(i, k, heart[heart_idx[(s + i) % HEART_ANIM_SIZE]][k]);
+            }
+        }
+        delay(HEART_TIME - (millis() - heart_time));
     }
 
-    // Blink LEDs
-    digitalWrite(LED1, resin % 2 == 0? HIGH:LOW);
-    digitalWrite(LED2, resin % 3 == 0? HIGH:LOW);
-    digitalWrite(LED3, resin % 5 == 0? HIGH:LOW);
+    // Blink LEDs for testing
+    digitalWrite(LED_S1, resin % 2 == 0? HIGH:LOW);
+    digitalWrite(LED_S2, resin % 3 == 0? HIGH:LOW);
+    digitalWrite(LED_CE, resin % 4 == 0? HIGH:LOW);
+    digitalWrite(LED_EX, resin % 5 == 0? HIGH:LOW);
 
+    // Increase resin for testing
     resin++;
-    if(resin > 99) resin = 0;
+    if(resin > 99) resin = 0; 
+    
+    // Clamp frame time
+    delay(FRAME_TIME - (millis() - frame_time));
 }
